3ea0e6231851fed91b51f9f489aa2e8787e6f951
7akacz7 <andrzej.klimalagit@gmail.com>
2023-06-10 16:03:41 +0200

    DodajWlasciciela

--- DodajPojazd.xaml.cs
+++ DodajPojazd.xaml.cs
@@ -27,6 +27,24 @@ namespace StacjaDiagnostyczna
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+            System.Windows.Data.CollectionViewSource pojazdViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("pojazdViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // pojazdViewSource.Źródło = [ogólne źródło danych]
+            System.Windows.Data.CollectionViewSource pojazdViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("pojazdViewSource1")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // pojazdViewSource1.Źródło = [ogólne źródło danych]
+            System.Windows.Data.CollectionViewSource pojazdViewSource2 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("pojazdViewSource2")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // pojazdViewSource2.Źródło = [ogólne źródło danych]
+            System.Windows.Data.CollectionViewSource wlascicielViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("wlascicielViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // wlascicielViewSource.Źródło = [ogólne źródło danych]
+            System.Windows.Data.CollectionViewSource wlascicielViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("wlascicielViewSource1")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // wlascicielViewSource1.Źródło = [ogólne źródło danych]
+            System.Windows.Data.CollectionViewSource wlascicielViewSource2 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("wlascicielViewSource2")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // wlascicielViewSource2.Źródło = [ogólne źródło danych]
         }
     }
 }
